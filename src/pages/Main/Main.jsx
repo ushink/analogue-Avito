a83051e55f385677ad67171f53f6e899b07148d6
Ulyana <ushinkevich@mail.ru>
2023-12-27 00:00:05 +0400

    chore: folder page

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,11 @@
+import s from './Main.module.css'
+
+function Main() {
+    return (
+        <div className={s.wrapper}>
+            <h1>Main</h1>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/pages/SellerProfile/SellerProfile.jsx
@@ -0,0 +1,10 @@
+import s from './SellerProfile.module.css'
+
+function SellerProfile() {
+    return (
+        <div className={s.wrapper}>
+            <h1>SellerProfile</h1>
+        </div>
+    )
+}
+export default SellerProfile
