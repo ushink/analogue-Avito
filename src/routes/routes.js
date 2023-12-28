07657b759f6f837f2cffdfd5a48072c482a7e6fb
Ulyana <ushinkevich@mail.ru>
2023-12-28 22:43:42 +0400

    feat: add seller profile page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,13 +2,15 @@ import Auth from '../pages/Auth/Auth'
 import Profile from '../pages/Profile/Profile'
 import Main from '../pages/Main/Main'
 import Adv from '../pages/Adv/Adv'
+import SellerProfile from '../pages/SellerProfile/SellerProfile'
 
 import {
     MAIN_ROUTE,
     LOGIN_ROUTE,
     REGISTRATION_ROUTE,
     PROFILE_ROUTE,
-    ADV_ROUTE
+    ADV_ROUTE,
+    SELLER_ROUTE
 } from '../utils/constants'
 
 export const publicRoutes = [
@@ -27,6 +29,10 @@ export const publicRoutes = [
     {
         element: <Adv />,
         path: ADV_ROUTE
+    },
+    {
+        element: <SellerProfile />,
+        path: SELLER_ROUTE
     }
 ]
 
