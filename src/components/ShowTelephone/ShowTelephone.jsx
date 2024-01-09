36a8cf0d149ae123a7ca746a71b0374475c5e47b
Ulyana <ushinkevich@mail.ru>
2024-01-09 07:32:01 +0400

    fix: show telephone seller profile page

--- src/components/ShowTelephone/ShowTelephone.jsx
+++ src/components/ShowTelephone/ShowTelephone.jsx
@@ -1,16 +1,16 @@
 import { useLocation } from 'react-router'
 import s from './ShowTelephone.module.css'
-import { ADV_ROUTE } from '../../utils/constants'
+import { MYADV_ROUTE } from '../../utils/constants'
 import Button from '../UI/Button/Button'
 
 function ShowTelephone() {
     const { pathname } = useLocation()
 
-    const advPage = pathname.slice(0, -2) === ADV_ROUTE.slice(0, -4)
+    const advPage = pathname.slice(0, -2) === MYADV_ROUTE.slice(0, -4)
 
     return (
         <>
-            {advPage ? (
+            {!advPage ? (
                 <div className={s.box}>
                     <h4 className={s.title}>Показать телефон</h4>
                     <p className={s.number}>8 905 ХХХ ХХ ХХ</p>
