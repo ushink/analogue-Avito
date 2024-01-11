88ada36733e1d31692a19619df19660e7696b7d9
Ulyana <ushinkevich@mail.ru>
2024-01-11 06:19:00 +0400

    feat: modal reviews user comments

--- src/components/modals/Reviews/Reviews.jsx
+++ src/components/modals/Reviews/Reviews.jsx
@@ -2,6 +2,7 @@
 import s from './Reviews.module.css'
 import { useState } from 'react'
 import Button from '../../UI/Button/Button'
+import { comments } from '../../../mock/comments'
 
 function Reviews({ setActive }) {
     const [reviews, setReviews] = useState('')
@@ -21,7 +22,7 @@ function Reviews({ setActive }) {
                 <form className={s.item}>
                     <label className={s.label}>Добавить отзыв</label>
                     <textarea
-                        className={s.reviews}
+                        className={s.inputReviews}
                         placeholder="Введите отзыв"
                         type="text"
                         onChange={(e) => {
@@ -30,6 +31,24 @@ function Reviews({ setActive }) {
                     ></textarea>
                 </form>
                 <Button color={'gray'}>{'Опубликовать'}</Button>
+
+                <div className={s.comments}>
+                    {comments.map((el) => (
+                        <div className={s.commentator} key={el.id}>
+                            <div className={s.photo}></div>
+                            <div className={s.personal}>
+                                <h3 className={s.name}>
+                                    {el.name}{' '}
+                                    <span className={s.data}>{el.data}</span>
+                                </h3>
+                                <div className={s.box}>
+                                    <h3 className={s.boxHeader}>Комментарий</h3>
+                                    <p className={s.boxReview}>{el.comment}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
---
+++ src/mock/comments.js
@@ -0,0 +1,58 @@
+export const comments = [
+    {
+        id: 1,
+        name: 'Олег',
+        data: 'Сегодня в 10:45',
+        comment:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    },
+    {
+        id: 2,
+        name: 'Олег',
+        data: 'Сегодня в 10:45',
+        comment:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    },
+    {
+        id: 3,
+        name: 'Олег',
+        data: 'Сегодня в 10:45',
+        comment:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    },
+    {
+        id: 4,
+        name: 'Олег',
+        data: 'Сегодня в 10:45',
+        comment:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    },
+    {
+        id: 5,
+        name: 'Олег',
+        data: 'Сегодня в 10:45',
+        comment:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    },
+    {
+        id: 6,
+        name: 'Олег',
+        data: 'Сегодня в 10:45',
+        comment:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    },
+    {
+        id: 7,
+        name: 'Олег',
+        data: 'Сегодня в 10:45',
+        comment:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    },
+    {
+        id: 8,
+        name: 'Олег',
+        data: 'Сегодня в 10:45',
+        comment:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+    }
+]
