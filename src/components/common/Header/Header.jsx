9775198ff445520b80bd54db435d510093f60426
Ulyana <ushinkevich@mail.ru>
2024-01-22 08:30:11 +0400

    fix: header mob auth page

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,12 +1,18 @@
 import s from './Header.module.css'
-import { LOGIN_ROUTE, PROFILE_ROUTE } from '../../../utils/constants'
-import { Link } from 'react-router-dom'
+import {
+    LOGIN_ROUTE,
+    PROFILE_ROUTE,
+    REGISTRATION_ROUTE
+} from '../../../utils/constants'
+import { Link, useLocation } from 'react-router-dom'
 import { useState } from 'react'
 import Modal from '../../UI/Modal/Modal'
 import NewAdv from '../../modals/NewAdv/NewAdv'
 import { useAuthSelector } from '../../../store/slice/auth'
 
 function Header() {
+    const { pathname } = useLocation()
+
     const { access } = useAuthSelector()
 
     const [modalActive, setModalActive] = useState(false)
@@ -16,28 +22,37 @@ function Header() {
             <div className={s.logoMob}>
                 <img src="../img/logoMob.svg" alt="" />
             </div>
-            <nav className={s.nav}>
-                {!access ? (
-                    <Link to={LOGIN_ROUTE}>
-                        <button className={s.btn}>Вход в личный кабинет</button>
-                    </Link>
-                ) : (
-                    <>
-                        <button
-                            className={s.btn}
-                            onClick={() => setModalActive(true)}
-                        >
-                            Разместить объявление
-                        </button>
-                        <Link to={PROFILE_ROUTE}>
-                            <button className={s.btn}>Личный кабинет</button>
+            {pathname !== LOGIN_ROUTE && pathname !== REGISTRATION_ROUTE && (
+                <nav className={s.nav}>
+                    {!access ? (
+                        <Link to={LOGIN_ROUTE}>
+                            <button className={s.btn}>
+                                Вход в личный кабинет
+                            </button>
                         </Link>
-                        <Modal active={modalActive} setActive={setModalActive}>
-                            <NewAdv setActive={setModalActive} />
-                        </Modal>
-                    </>
-                )}
-            </nav>
+                    ) : (
+                        <>
+                            <button
+                                className={s.btn}
+                                onClick={() => setModalActive(true)}
+                            >
+                                Разместить объявление
+                            </button>
+                            <Link to={PROFILE_ROUTE}>
+                                <button className={s.btn}>
+                                    Личный кабинет
+                                </button>
+                            </Link>
+                            <Modal
+                                active={modalActive}
+                                setActive={setModalActive}
+                            >
+                                <NewAdv setActive={setModalActive} />
+                            </Modal>
+                        </>
+                    )}
+                </nav>
+            )}
         </header>
     )
 }
